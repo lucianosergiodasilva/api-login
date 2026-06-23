1aa8d743cf54483a4e010028a1921d20fe605b57
lucianosergiodasilva <lucianomirabolant@gmail.com>
2026-06-23 17:02:49 -0300

    Nova branch api

---
+++ backend/comentarios.js
@@ -0,0 +1,18 @@
+// MongoDB:
+
+// username
+// lucianomirabolant_db_user
+
+// senha
+// r5If6bAZNhIYeRcm
+
+// string de conexão
+// mongodb+srv://lucianomirabolant_db_user:r5If6bAZNhIYeRcm@cluster0.mblegwq.mongodb.net/?appName=Cluster0
+
+// instalar:
+// express
+// dotenv
+// prisma
+// jwt
+// prisma client
+// bcrypt
